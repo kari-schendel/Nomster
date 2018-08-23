9d1c60070b104bf090854970281597923c8c9b52
Kari Schendel <firefaded@gmail.com>
2018-08-23 03:57:56 +0000

    added comments to database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, length: { minimum: 3}
   validates :name, :description, :address, presence: true
 end
