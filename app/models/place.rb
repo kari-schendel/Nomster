f7ad9c3fc9e3b544cb918702da83a60f3194cf0e
Kari Schendel <firefaded@gmail.com>
2018-08-23 03:07:16 +0000

    added google API key for geoooding

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+  
   validates :name, length: { minimum: 3}
   validates :name, :description, :address, presence: true
 end
