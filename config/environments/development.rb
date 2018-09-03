461f254a955a58424ba42e94527a5efe285ab4e0
Kari Schendel <firefaded@gmail.com>
2018-09-03 02:39:50 +0000

    added auto-email with comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,15 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :test
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-   port: 587,
-   domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "example.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_ADDRESS'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
 
   config.action_mailer.default_url_options = { host: 'localhost: 3030'}
   # Settings specified here will take precedence over those in config/application.rb.
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
