81b2fd8ac2a54092e61b68e7a6d280f676ec743a
Kari Schendel <firefaded@gmail.com>
2018-09-03 02:30:20 +0000

    adjusted subject line and user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,8 +3,8 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
+   port: 587,
+   domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_ADDRESS'],
